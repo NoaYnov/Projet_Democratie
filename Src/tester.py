b2aaf32018efd5b67a38760e7d5aea4e9ce11139
gambeynoa <113116077+NoaYnov@users.noreply.github.com>
2024-12-14 15:51:25 +0100

    API_recup
    Début de mise en place des fonctions de récupération de l'api et des datasets

--- Src/tester.py
+++ Src/tester.py
@@ -1,55 +1,74 @@
-# # recupere une donnée de mongodb
-
-# import pymongo
-
-# def get_data_from_mongodb():
-#     url = "mongodb+srv://Noa:Griffith@operation-eclipse.cqvrd.mongodb.net/"
-#     client = pymongo.MongoClient(url)
-#     db = client['test1']
-#     collection = db['guts']
-#     data = collection.find_one()
-#     return data
-
-# data = get_data_from_mongodb()
-# print(data['test'])
-
-
-# fetch data from api https://tabular-api.data.gouv.fr/api/resources/1c5075ec-7ce1-49cb-ab89-94f507812daf/ and whrite the data in a file
-
 import requests
 import pandas as pd
+from io import StringIO
+import os
+import requests
+import unicodedata
 
-def fetch_data_from_api():
-    url = "https://tabular-api.data.gouv.fr/api/resources/b8703c69-a18f-46ab-9e7f-3a8368dcb891/data/csv/"
+
+
+# all_requests = disctionary
+
+all_requests = {
+    'Pre_2022_T1': '79b5cac4-4957-486b-bbda-322d80868224',
+    'Pre_2022_T2': '4dfd05a9-094e-4043-8a19-43b6b6bbe086'
+    }
+
+
+
+
+def remove_accents_and_special_chars(text):
+    # Normalise le texte pour retirer les accents et autres caractères spéciaux
+    nfkd_form = unicodedata.normalize('NFKD', text)
+    return ''.join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+def fetch_data_from_api(data,name):
+    url = f"https://www.data.gouv.fr/fr/datasets/r/{data}"
     response = requests.get(url)
-    data = response.json()
-    return data
-
-def write_data_to_file(data, filename='csv/data.csv'):
-    with open(filename, mode='w', encoding='utf-8') as file:
-        file.write(data)
-        
-        
-def normalize_data(data):
-    data = fetch_data_from_api()
-    # replace ' with " in the data and , with ,\n
-    data = str(data).replace("'", '"')
-    return data
     
+    if response.status_code != 200:
+        print(f"Erreur {response.status_code}: La requête a échoué.")
+        return None
+    
+    # Vérifie si la réponse est bien du contenu textuel
+    if 'text/plain' in response.headers.get('Content-Type', ''):
+        print("La réponse est au format txt.")
         
-# def json_to_csv(data):
-#     write_data_to_file(data)
-#     df = pd.read_json('data.json')
-#     df.to_csv('csv/data.csv', index=False)
-#     return df
+        # Retirer les accents et les caractères spéciaux
+        clean_text = remove_accents_and_special_chars(response.text)
         
-        
+        # Enregistrer le texte nettoyé dans un fichier .txt
+        with open(f'csv/{name}.txt', 'w', encoding='utf-8') as file:
+            file.write(clean_text)
+            
+        print("Le fichier txt a été enregistré sans accents ni caractères spéciaux.")
+        return None  # On ne retourne pas de données ici, car il s'agit d'un fichier txt
+    return None
+
+
+def txt_to_csv(name):
+    # Convertir le fichier txt en csv
+    data = pd.read_csv(f'csv/{name}.txt', sep=';')
+    data.to_csv(f'csv/{name}.csv', index=False)
+    print("Le fichier txt a été converti en csv.")
+    # delete file
+    os.remove(f'csv/{name}.txt')
+
 
 def main():
-    data = normalize_data(fetch_data_from_api())
-    write_data_to_file(data)
-    # json_to_csv(data)
-    
-    
+    for name in all_requests:
+        # if file exist pass
+        if os.path.isfile(f'csv/{name}.csv'):
+            print(f"Le fichier {name}.csv existe déjà.")
+            pass
+        else:
+            fetch_data_from_api(all_requests[name], name)
+            txt_to_csv(name)
+
+
+
+
 if __name__ == '__main__':
     main()
+
+
